5f284479d69f9a53d63b03bc4bf9082be57c0c98
roppy001 <roppy.dreamvision200@gmail.com>
2021-01-07 00:41:46 +0900

    第1回大富豪決定戦ランキング対応

---
+++ src/main/java/roppy/dq10/rankanalytics/scraper/DaifugoMatchScraper.java
@@ -0,0 +1,57 @@
+package roppy.dq10.rankanalytics.scraper;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import roppy.dq10.rankanalytics.scraper.dto.RankItem;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DaifugoMatchScraper implements Scraper{
+    private static final int PAGE_NUMBER = 20;
+
+    @Override
+    public List<List<RankItem>> scrape(int round, int subraceNumber) throws IOException,ScrapingException {
+        if(subraceNumber != 1){
+            throw new ScrapingException("Illegal subrace number.");
+        }
+
+        List<List<RankItem>> listList = new ArrayList<>();
+
+        List<RankItem> list = new ArrayList<>();
+        listList.add(list);
+
+        for(int page = 0; page < PAGE_NUMBER;page++) {
+
+            String url = String.format("https://hiroba.dqx.jp/sc/worldRanking/daifugoRanking/%d/page/%d",round,page);
+
+            Document document = Jsoup.connect(url).get();
+            Elements rankers = document.select("tr.firstrank,tr.secondrank,tr.thirdrank,tr.rank");
+            for (Element ranker : rankers) {
+                RankItem r = new RankItem();
+                int rank;
+                try{
+                    rank = Integer.parseInt(ranker.select(".col1 .rank").get(0).html().replace("位", "").trim());
+                }catch (NumberFormatException nfe){
+                    throw new ScrapingException(nfe);
+                }
+                r.setRank(rank);
+                r.setName(ranker.select(".col2").get(0).html().trim());
+                r.setExtraData("");
+                int point;
+                try{
+                    point = Integer.parseInt(ranker.select(".col3 .text-right").get(0).html().replace("P", "").trim());
+                }catch (NumberFormatException nfe){
+                    throw new ScrapingException(nfe);
+                }
+                r.setPoint(point);
+
+                list.add(r);
+            }
+        }
+        return listList;
+    }
+}
