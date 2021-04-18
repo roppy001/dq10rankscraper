25d71ae2eee2df6238cdd07209e1c6b41688a714
roppy001 <roppy.dreamvision200@gmail.com>
2021-04-18 21:03:57 +0900

    カジノレイド　ビンゴに対応

--- src/main/java/roppy/dq10/rankanalytics/scraper/RaceConfig.java
+++ src/main/java/roppy/dq10/rankanalytics/scraper/RaceConfig.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum RaceConfig {
     SLIME_RACE("slimerace","roppy.dq10.rankanalytics.scraper.SlimeRaceScraper","slimerace",1,UpdateTimeConfig.EIGHT_HOURS),
     BATTLE_TRINITY("trinity","roppy.dq10.rankanalytics.scraper.BattleTrinityScraper","trinity",6,UpdateTimeConfig.EIGHT_HOURS),
-    CASINO_RAID("casinoraid","roppy.dq10.rankanalytics.scraper.CasinoRaidScraper","casinoraid",3,UpdateTimeConfig.EIGHT_HOURS),
+    CASINO_RAID_1("casinoraid1","roppy.dq10.rankanalytics.scraper.CasinoRaidScraper","casinoraid",3,UpdateTimeConfig.EIGHT_HOURS),
+    CASINO_RAID_2("casinoraid2","roppy.dq10.rankanalytics.scraper.CasinoRaidScraper","casinoraid",4,UpdateTimeConfig.EIGHT_HOURS),
     BATTLE_PENCIL("pencil","roppy.dq10.rankanalytics.scraper.BattlePencilScraper","pencil",1,UpdateTimeConfig.EIGHT_HOURS),
 //    COLOSSEUM("colosseum","roppy.dq10.rankanalytics.scraper.ColosseumScraper","colosseum",1),
     DAIFUGO("daifugo","roppy.dq10.rankanalytics.scraper.DaifugoScraper","daifugo",1,UpdateTimeConfig.ONE_DAY),
